ad0fc5feaf94f1d0511ce226ecfff9dc00f20860
emazack <em.zaccaria@gmail.com>
2022-10-08 19:15:29 +0200

    Finish the challange

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+
 
 function App() {
+  
+  const [quantity, setQuantity] = useState<number>(1);
+  
+  
+  const increase = (): void => {
+    if (quantity < 5) {      
+      setQuantity(quantity + 1);
+    }
+  }
+
+  const decrease = (): void => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  }
+
+  const order = (): void => {
+    alert(`Your total order is ${10 * quantity} `)
+  }
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-2" style={{ maxWidth: 400}}>
+      <div className="card">
+        <div className="card-header">MY AMAZING PRODUCT</div>
+        <div className="card-body">
+          <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+          <br/>
+          <div className="btn-group">
+            <button onClick={increase} className="btn btn-outline-primary">+</button>
+            <button onClick={decrease} className="btn btn-outline-primary">-</button>
+          </div>
+          <hr/>
+          <button className="btn btn-primary" onClick={order}>
+            Order {quantity} product{quantity > 1 && <span>s</span>} at € {10 * quantity}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
